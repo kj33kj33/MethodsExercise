f50ad4cd0ee3096a6371e7e263decbb7fb75a716
James <jamesmcdaniel33@hotmail.com>
2023-05-12 09:43:24 -0500

    encapsulated exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -16,36 +16,40 @@
         public static int Add(int num1, int num2)
         {
             var answer = num1 + num2;
+            Console.WriteLine("Sum: " + answer);
             return answer;
         }
 
         public static int Subtract(int num1, int num2)
         {
             var answer = num1 - num2;
+            Console.WriteLine("Difference: " + answer);
             return answer;            
         }
 
         public static int Multiply(int num1, int num2)
         {
             var answer = num1 * num2;
+            Console.WriteLine("Product: " + answer);
             return answer;
         }
 
         public static int Divide(int num1, int num2)
         {
             var answer = num1 / num2;
+            Console.WriteLine("Quotient: " + answer);
             return answer;
         }
 
         public static int Modulus(int num1, int num2)
         {
             var answer = num1 % num2;
+            Console.WriteLine("Remainder: " + answer);
             return answer;
         }
 
-        static void Main(string[] args)
+        static void ExerciseOne()
         {
-
             Console.WriteLine("What is your first name?");
             var userName = Console.ReadLine();
 
@@ -61,21 +65,34 @@
             Console.WriteLine($"Once upon a time, there was a person named {userName}. They loved the color {color}, adored {animal}, and couldn't get enough of {band}'s music.");
             Console.WriteLine($"One day, while {userName} was out and about, they stumbled upon a magical moment. They encountered a {animal} that seemed to dance to the rhythm of {band}'s tunes. It was a sight that filled {userName} with joy.");
             Console.WriteLine($"From that day on, whenever {userName} saw their favorite color, {color}, heard their favorite band, {band}, or encountered their favorite animal, {animal}, they couldn't help but smile and remember that magical dance they witnessed.");
+        }
 
-            int answer1 = Add(33, 50);
-            int answer2 = Subtract(100, 33);
-            int answer3 = Multiply(3, 10);
-            int answer4 = Divide(15, 3);
-            int answer5 = Modulus(14, 3);
+        static void ExerciseTwo()
+        {
+            Console.WriteLine("Pick a number.");
+            var num1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Sum: " + answer1);
-            Console.WriteLine("Difference: " + answer2);
-            Console.WriteLine("Product: " + answer3);
-            Console.WriteLine("Quotient: " + answer4);
-            Console.WriteLine("Remainder: " + answer5);
+            Console.WriteLine("Pick another number.");
+            var num2 = int.Parse(Console.ReadLine());
+
+            Add(num1, num2);
+
+            Subtract(num1, num2);
+
+            Multiply(num1, num2);
+
+            Divide(num1, num2);
+
+            Modulus(num1, num2);
 
             AddNumbers(1, 1, 1, 1, 1);
+        }
 
+        static void Main(string[] args)
+        {
+            ExerciseOne();
+
+            ExerciseTwo();
         }
     }
 }
